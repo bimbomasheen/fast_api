9e7a63b6f2c178d6f49f8f66716b5f7290d09fb1
Lasse Lorentzen <lwlorentz@gmail.com>
2024-06-21 18:35:58 +0200

    renamed file to main

--- main.py
+++ main.py
@@ -29,29 +29,29 @@ def get_items():
     return inventory
 
 @app.get("/items/{item_id}")
-def get_item_by_id(item_id: int = Path(title="Item ID", description="The ID of the item.")):
+def get_item_by_id(item_id: int = Path(title="Item ID", description="The ID of the item.")) -> Item:
     if item_id not in inventory:
-        raise HTTPException(status_code=404, detail="Item not found.")
+        raise HTTPException(status_code=404, detail=f"Item with {item_id=} not found.")
     return inventory[item_id]
 
 @app.get("/items/by-name/")
-def get_item_by_name(name: str = Query(title="Name", description="The name of the item.")):
+def get_item_by_name(name: str = Query(title="Name", description="The name of the item.")) -> Item:
     for item_id in inventory:
         if inventory[item_id].name == name:
             return inventory[item_id]
-    raise HTTPException(status_code=404, detail="Item not found.")
+    raise HTTPException(status_code=404, detail=f"Item with {name=} not found.")
 
 @app.post("/items/{item_id}")
-def create_item(item_id: int, item: Item):
+def create_item(item_id: int, item: Item) -> Item:
     if item_id in inventory:
-        raise HTTPException(status_code=400, detail="Item already exists.")
+        raise HTTPException(status_code=400, detail=f"Item with {item_id=} already exists.")
     inventory[item_id] = item
     return inventory[item_id]
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: UpdateItem):
+def update_item(item_id: int, item: UpdateItem) -> Item:
     if item_id not in inventory:
-        raise HTTPException(status_code=404, detail="Item not found.")
+        raise HTTPException(status_code=404, detail=f"Item with {item_id=} not found.")
     if item.name:
         inventory[item_id].name = item.name
     if item.price:
@@ -61,8 +61,8 @@ def update_item(item_id: int, item: UpdateItem):
     return inventory[item_id]
 
 @app.delete("/items/{item_id}")
-def delete_item(item_id: int):
+def delete_item(item_id: int) -> dict:
     if item_id not in inventory:
-        raise HTTPException(status_code=404, detail="Item not found.")
+        raise HTTPException(status_code=404, detail=f"Item with {item_id=} not found.")
     del inventory[item_id]
     return {"Message": "Item deleted."}
